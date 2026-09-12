d186a4be85ea3d8648958bc7b6110fa70425a011
mayureshmadule-afk <mayureshmadule@gmail.com>
2026-09-12 12:38:44 +0530

    Update server.js
    Mythic

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const http = require("http");
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {
+    "Content-Type": "application/json"
+  });
+
+  res.end(JSON.stringify({
+    answer: "Hello! Mythic AI is connected 🤖"
+  }));
+});
+
+server.listen(3000);
